ce33e145c2c1a38f63db860e089bfee5ebc0ec4d
HuyPQ <huyhipvp9x@gmail.com>
2020-12-18 23:05:37 +0700

    doing track selector

--- app/src/main/java/com/kohuyn/exoplayerexample/MainActivity.kt
+++ app/src/main/java/com/kohuyn/exoplayerexample/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.kohuyn.exoplayerexample
 
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector.ParametersBuilder
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Player.EventListener {
+
+    companion object {
+        const val KEY_TRACK_SELECTOR_PARAMETERS = "track_selector_parameters"
+        const val KEY_WINDOW = "window"
+        const val KEY_POSITION = "position"
+        const val KEY_AUTO_PLAY = "auto_play"
+    }
+
+    private val playerView by lazy { player_view }
+
+    private lateinit var simpleExoplayer: SimpleExoPlayer
+    private var playbackPosition: Long = 0
+
+    private val dataSourceFactory: DataSource.Factory by lazy {
+        DefaultDataSourceFactory(this, "exoplayer-sample")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            playbackPosition = savedInstanceState.getLong(KEY_POSITION)
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong(KEY_POSITION, playbackPosition)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initializePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    private fun initializePlayer() {
+        simpleExoplayer = ExoPlayerFactory.newSimpleInstance(this)
+        preparePlayer("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+        playerView.player = simpleExoplayer
+        simpleExoplayer.seekTo(playbackPosition)
+        simpleExoplayer.playbackParameters = PlaybackParameters(5f)
+        simpleExoplayer.playWhenReady = true
+        simpleExoplayer.addListener(this)
+    }
+
+    private fun preparePlayer(videoUrl: String) {
+        val uri = Uri.parse(videoUrl)
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(uri)
+        simpleExoplayer.prepare(mediaSource)
+    }
+
+    private fun releasePlayer() {
+        playbackPosition = simpleExoplayer.currentPosition
+        simpleExoplayer.release()
+    }
+
+    override fun onPlayerError(error: ExoPlaybackException) {
+        // handle error
+    }
+
 }
